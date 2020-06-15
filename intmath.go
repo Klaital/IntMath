c6e02a76a3cbaa02ceb85b0b4d4726de4bf061b0
Kit Cox <chriscox@abandonedfactory.net>
2020-06-15 15:23:47 -0700

    Adding Max funcs

--- intmath.go
+++ intmath.go
@@ -13,3 +13,18 @@ func Abs(x int64) int64 {
 
 	return -x
 }
+
+func Max(x int64, y int64) int64 {
+	if x >= y {
+		return x
+	}
+	return y
+}
+
+func MaxUint64(x uint64, y uint64) uint64 {
+	if x >= y {
+		return x
+	}
+	return y
+}
+
--- intmath_test.go
+++ intmath_test.go
@@ -22,3 +22,27 @@ func TestAbs(t *testing.T) {
 		t.Errorf("Failed to operate on MinInt64. Expected MaxInt (%d), got %d", math.MaxInt64, x)
 	}
 }
+
+func TestMax(t *testing.T) {
+	if x := Max(0, 1); x != 1 {
+		t.Errorf("Failed to operate on (small, large). Expected %d, got %d", 1, x)
+	}
+	if x := Max(5, 5); x != 5 {
+		t.Errorf("Failed to operate on (equal, equal). Expected %d, got %d", 5, x)
+	}
+	if x := Max(6, 5); x != 6 {
+		t.Errorf("Failed to operate on (large, small). Expected %d, got %d", 6, x)
+	}
+}
+
+func TestMaxUint64(t *testing.T) {
+	if x := MaxUint64(0, 1); x != 1 {
+		t.Errorf("Failed to operate on (small, large). Expected %d, got %d", 1, x)
+	}
+	if x := MaxUint64(5, 5); x != 5 {
+		t.Errorf("Failed to operate on (equal, equal). Expected %d, got %d", 5, x)
+	}
+	if x := MaxUint64(6, 5); x != 6 {
+		t.Errorf("Failed to operate on (large, small). Expected %d, got %d", 6, x)
+	}
+}
